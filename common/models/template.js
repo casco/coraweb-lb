3852b28785316c0ca08e635cd7707f7be912d302
casco <alejandro.casco.fernandez@gmail.com>
2017-05-25 15:00:25 -0300

    a bit closer

--- common/models/template.js
+++ common/models/template.js
@@ -1,6 +1,17 @@
 'use strict';
 
+var Regex=require("regex");
+
 module.exports = function(Template) {
+
+/**
+ * Returns true if the receiver matchers the url passed as an argument
+ * @param {string} url A URL
+ */
+Template.prototype.matches = function(url) {
+    var regex = new Regex(this.urlPattern);
+    return regex.test(url)
+}
 
  /**
  * Returns all templates that match the url passed as an argument
@@ -8,8 +19,13 @@ module.exports = function(Template) {
  * @param {Function(Error)} callback
  */
 Template.findAllMatching = function(url, callback) {
-  // TODO
-  callback(null);
+  Template.find(function(err, returned_instances) {
+    var filtered = returned_instances
+     //filtered = returned_instances.filter(function(x) {return x.matches(url)});
+    var response = "number of instances " + filtered.length;
+    console.log(filtered);
+    callback(null, response);
+  });
 };
 
 };
---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function(app) {
+  app.dataSources.db.automigrate('template', function(err) {
+    if (err) throw err;
+    app.models.Template.create([
+        {
+            "groups": ["public"],
+            "name" : "a simple template",
+            "itemType": "http://schema.org/VideoObject",
+            "urlPattern": "https://www.youtube.com/watch.*",
+            "xPathPropertySelectors": {
+                "title": "//span[@id=\"eow-title\"]/text()",
+                "description": "//p[@id=\"eow-description\"]/text()"
+            } 
+        }
+        ], function(err, templates) {
+      if (err) throw err;
+      console.log('Models created: \n', templates);
+    });
+  });
+};
