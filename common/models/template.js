9e032876ba7fdb97831e2024bb3318342101ea30
casco <alejandro.casco.fernandez@gmail.com>
2017-05-25 11:27:48 -0300

    Initial version - most of it generated by Loopback CLI

---
+++ common/models/template.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function(Template) {
+
+ /**
+ * Returns all templates that match the url passed as an argument
+ * @param {string} url A URL
+ * @param {Function(Error)} callback
+ */
+Template.findAllMatching = function(url, callback) {
+  // TODO
+  callback(null);
+};
+
+};
