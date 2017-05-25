c09ece72b4295f381777f7b63377c6ad2a7d4320
casco <alejandro.casco.fernandez@gmail.com>
2017-05-25 20:01:14 -0300

    matches almost ready

--- common/models/template.js
+++ common/models/template.js
@@ -4,28 +4,34 @@ var Regex=require("regex");
 
 module.exports = function(Template) {
 
-/**
- * Returns true if the receiver matchers the url passed as an argument
- * @param {string} url A URL
- */
-Template.prototype.matches = function(url) {
-    var regex = new Regex(this.urlPattern);
-    return regex.test(url)
-}
+  /**
+   * Returns true if the receiver matchers the url passed as an argument
+   * @param {string} url A URL
+   */
+  Template.prototype.matches = function(url) {
+      var regex = new Regex(this.urlPattern);
+      return regex.test(url)
+  }
 
- /**
- * Returns all templates that match the url passed as an argument
- * @param {string} url A URL
- * @param {Function(Error)} callback
- */
-Template.findAllMatching = function(url, callback) {
-  Template.find(function(err, returned_instances) {
-    var filtered = returned_instances
-     //filtered = returned_instances.filter(function(x) {return x.matches(url)});
-    var response = "number of instances " + filtered.length;
-    console.log(filtered);
-    callback(null, response);
+  /**
+   * Returns all templates that match the url passed as an argument
+   * @param {string} url A URL
+   * @param {Function(Error)} callback
+   */
+  Template.findAllMatching = function(url, callback) {
+    Template.find(function(err, returned_instances) {
+      var filtered = returned_instances.filter(function(x) {return x.matches(url)});
+      //var filtered = returned_instances;
+      console.log("number of instances " + filtered.length);
+      console.log(filtered);
+      callback(null, filtered);
+    });
+  };
+
+  Template.remoteMethod ('findAllMatching', {
+            http: {path: '/matching', verb: 'get'},
+            accepts: {arg: 'url', type: 'string'},
+            returns: {arg: 'response', type: 'array'}
   });
-};
 
 };
