daf757d407da89889a588f1b9ad6f7420ea71f95
casco <alejandro.casco.fernandez@gmail.com>
2017-05-26 21:50:24 -0300

    extractors for mendeley, slideshare, and youtube

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -6,12 +6,33 @@ module.exports = function (app) {
     app.models.Template.create([
       {
         "groups": ["public"],
-        "name": "a simple template",
+        "name": "Youtube videos",
         "itemType": "http://schema.org/VideoObject",
         "urlPattern": "https://www.youtube.com/watch.*",
         "xPathPropertySelectors": {
           "title": "[id=eow-title]",
           "description": "[id=eow-description]"
+        }
+      },
+      {
+        "groups": ["public"],
+        "name": "Slideshare presentation",
+        "itemType": "http://schema.org/PresentationDigitalDocument",
+        "urlPattern": "https://www.slideshare.net/.*",
+        "xPathPropertySelectors": {
+          "title": "[class=j-title-breadcrumb]",
+          "description": "[id=slideshow-description-paragraph]"
+        }
+      },
+      {
+        "groups": ["public"],
+        "name": "Mendeley article presentation",
+        "itemType": "http://schema.org/Article",
+        "urlPattern": "https://www.mendeley.com/research-papers/.*",
+        "xPathPropertySelectors": {
+          "title": "[data-name=title]",
+          "authors": "[class=authors]",
+          "description": "[data-name=content]"
         }
       }
     ], function (err, templates) {
