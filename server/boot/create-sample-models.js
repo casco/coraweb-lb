218b2980e38110e1cc5f08bacadbf14cca058ea5
casco <alejandro.casco.fernandez@gmail.com>
2017-05-30 19:31:33 -0300

    -email is now primary kye of Person -fixed relation/foreing-keys in Template and Item

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,43 +1,61 @@
 'use strict';
 
+var samplePeople = [
+  {
+    "fullName": "Anonymous",
+    "email": "no_reply@lifia.info.unlp.edu.ar",
+    "groups": ["lifia", "public"]
+  }
+]
+
+var sampleTemplates = [
+  {
+    "groups": ["public"],
+    "name": "Youtube videos",
+    "itemType": "http://schema.org/VideoObject",
+    "urlPattern": "https://www.youtube.com/watch.*",
+    "owner": "no_reply@lifia.info.unlp.edu.ar",
+    "xPathPropertySelectors": {
+      "title": "[id=eow-title]",
+      "description": "[id=eow-description]"
+    }
+  },
+  {
+    "groups": ["public"],
+    "name": "Slideshare presentation",
+    "itemType": "http://schema.org/PresentationDigitalDocument",
+    "urlPattern": "https://www.slideshare.net/.*",
+    "owner": "no_reply@lifia.info.unlp.edu.ar",
+    "xPathPropertySelectors": {
+      "title": "[class=j-title-breadcrumb]",
+      "description": "[id=slideshow-description-paragraph]"
+    }
+  },
+  {
+    "groups": ["public"],
+    "name": "Mendeley article presentation",
+    "itemType": "http://schema.org/Article",
+    "urlPattern": "https://www.mendeley.com/research-papers/.*",
+    "owner": "no_reply@lifia.info.unlp.edu.ar",
+    "xPathPropertySelectors": {
+      "title": "[data-name=title]",
+      "authors": "[class=authors]",
+      "description": "[data-name=content]"
+    }
+  }
+];
+
 module.exports = function (app) {
-  app.dataSources.db.automigrate('Template', function (err) {
+  var ds = app.dataSources.db;
+  ds.automigrate(function (err) {
     if (err) throw err;
-    app.models.Template.create([
-      {
-        "groups": ["public"],
-        "name": "Youtube videos",
-        "itemType": "http://schema.org/VideoObject",
-        "urlPattern": "https://www.youtube.com/watch.*",
-        "xPathPropertySelectors": {
-          "title": "[id=eow-title]",
-          "description": "[id=eow-description]"
-        }
-      },
-      {
-        "groups": ["public"],
-        "name": "Slideshare presentation",
-        "itemType": "http://schema.org/PresentationDigitalDocument",
-        "urlPattern": "https://www.slideshare.net/.*",
-        "xPathPropertySelectors": {
-          "title": "[class=j-title-breadcrumb]",
-          "description": "[id=slideshow-description-paragraph]"
-        }
-      },
-      {
-        "groups": ["public"],
-        "name": "Mendeley article presentation",
-        "itemType": "http://schema.org/Article",
-        "urlPattern": "https://www.mendeley.com/research-papers/.*",
-        "xPathPropertySelectors": {
-          "title": "[data-name=title]",
-          "authors": "[class=authors]",
-          "description": "[data-name=content]"
-        }
-      }
-    ], function (err, templates) {
+    app.models.Person.create(samplePeople, function (err, people) {
       if (err) throw err;
-      console.log('Models created: \n', templates);
+      console.log('People created: \n', people);
+    });
+    app.models.Template.create(sampleTemplates, function (err, templates) {
+      if (err) throw err;
+      console.log('Templates created: \n', templates);
     });
   });
 };
